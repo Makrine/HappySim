7480ba1fca350cfaf1131aa562e39aad30401b5e
Makrina Tskipurishvili <66353680+Makrine@users.noreply.github.com>
2023-04-14 16:35:54 +0400

    create a rigged character

--- Assets/Scripts/ShopKeeper/ShopKeeper.cs
+++ Assets/Scripts/ShopKeeper/ShopKeeper.cs
@@ -1,6 +1,7 @@
 using UnityEngine;
 using ShopSystem;
 using System;
+using UnityEngine.UI;
 
 public class ShopKeeper : MonoBehaviour
 {
@@ -8,6 +9,9 @@ public class ShopKeeper : MonoBehaviour
     public Inventory inventory;
     public Cart cart;
     public PlayerInvenotry playerInventory;
+
+    // the message that pops up when the player interacts with the shopkeeper
+    public Transform messagePopup;
 
     public EventHandler onShopOpen;
     public EventHandler onShopClose;
@@ -21,16 +25,7 @@ public class ShopKeeper : MonoBehaviour
     {
         if(other.CompareTag("Player"))
         {
-            // Get the inventory of the player who entered the shop
-            // using this, we will be able to sell items to the player
-            // who enters the shop
-            playerInventory = other.GetComponent<PlayerInvenotry>();
-            // Set the items container of the player inventory to the items container of the shop inventory
-            // This way the inventory UI will show the items of the player
-            playerInventory.itemsContainer = inventory.itemsContainer;
-            shop.OpenShop(true);
-            inventory.OpenInventory(true);
-            onShopOpen?.Invoke(this, EventArgs.Empty);
+            ShowPopup(true);
         }
     }
 
@@ -38,9 +33,50 @@ public class ShopKeeper : MonoBehaviour
     {
         if(other.CompareTag("Player"))
         {
+            ShowPopup(false);
             shop.OpenShop(false);
             inventory.OpenInventory(false);
             onShopClose?.Invoke(this, EventArgs.Empty);
         }
+    }
+
+    private void ShowPopup(bool flag)
+    {
+        messagePopup.gameObject.SetActive(flag);
+    }
+
+    private void OpenShop(Collider2D other)
+    {
+        // Get the inventory of the player who entered the shop
+        // using this, we will be able to sell items to the player
+        // who enters the shop
+        playerInventory = other.GetComponent<PlayerInvenotry>();
+        // Set the items container of the player inventory to the items container of the shop inventory
+        // This way the inventory UI will show the items of the player
+        playerInventory.itemsContainer = inventory.itemsContainer;
+        shop.OpenShop(true);
+        inventory.OpenInventory(true);
+        onShopOpen?.Invoke(this, EventArgs.Empty);
+
+    }
+
+    [SerializeField] private Button button;
+    [SerializeField] private float offsetX;
+    [SerializeField] private float offsetY;
+
+    private RectTransform buttonRectTransform;
+
+    void Start()
+    {
+        // Get the RectTransform component of the button:
+        buttonRectTransform = button.GetComponent<RectTransform>();
+    }
+
+    void LateUpdate()
+    {
+        // Get the screen position of the game object:
+        Vector3 screenPos = Camera.main.WorldToScreenPoint(transform.position);
+        // Set the position of the button's RectTransform component:
+        buttonRectTransform.position = new Vector3(screenPos.x + offsetX, screenPos.y + offsetY, 0);
     }
 }
